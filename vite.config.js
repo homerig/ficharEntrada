cad2aee79d70d4f14251b32f09cb8adb59b24bac
homerig <homerogonzalez2009@gmail.com>
2026-03-13 18:53:57 -0300

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:"/ficharEntrada/",
+  base: "/",
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
   server: {
     proxy: {
